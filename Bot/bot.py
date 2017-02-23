85e3290d2817c624049c3b5c8ac757758b056665
Suraj Pai <surajballambat@gmail.com>
2017-02-24 01:18:30 +0530

    repeat previous move

--- Bot/bot.py
+++ Bot/bot.py
@@ -5,26 +5,34 @@ import numpy as np
 
 class Bot:
 
-
     def __init__(self):
         self.game = None
+        self.previous = []
 
     def setup(self, game):
         self.game = game
 
-    def mimic_opponent(self):
-        [height, width] = self.game.field_height, self.game.field_width
-        field = self.game.field
-        field = np.reshape(field, (height, width))
-	
+    def repeat_previous_move(self, legal):
+        if len(self.previous) == 0:
+            (_, choice) = random.choice(legal)
+            self.previous.append(choice)
+            return choice
+
+        else:
+            last_move = self.previous[-1]
+            for ((row, col), move) in legal:
+                if last_move == move:
+                    return last_move
+
+            (_, choice) = random.choice(legal)
+            return choice
+
     def do_turn(self):
-
-
         legal = self.game.field.legal_moves(self.game.my_botid, self.game.players)
         #self.game.field.output()
         if len(legal) == 0:
             self.game.issue_order_pass()
         else:
-            (_, choice) = random.choice(legal)
+            choice = self.repeat_previous_move(legal)
             self.game.issue_order(choice)
 
