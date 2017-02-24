54f0457cd067f5033f043c109473469d0e2acb48
Suraj Pai <surajballambat@gmail.com>
2017-02-25 03:33:24 +0530

    updated to give reward for being closer to snippets

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,32 +1,41 @@
 import random
 import sys
-
+import numpy as np
 
 class Bot:
 
     def __init__(self):
         self.game = None
-        self.previous = None
-        self.repeat_count = 0
 
     def setup(self, game):
         self.game = game
 
-    def repeat_previous_move(self, legal):
-        if self.previous is None:
-            (_, choice) = random.choice(legal)
-            return choice
+    def distance_metric(self, vector1 , vector2):
+        vector1mag = np.sqrt(vector1[0] ** 2 + vector1[1] ** 2)
+        vector2mag = np.sqrt(vector2[0] ** 2 + vector2[1] ** 2)
+        distance = np.abs(vector1mag - vector2mag)
+        return distance
 
-        elif self.repeat_count <= 3:
-            last_move = self.previous
-            for ((row, col), move) in legal:
-                if last_move == move:
-                    self.repeat_count += 1
-                    return last_move
-        self.repeat_count = 0
+    def get_grid(self):
+        global opponent_loc
+        global my_loc
+        field = np.array(self.game.field.cell)
+        [field_height, field_width] = self.game.field_height, self.game.field_width
+        field = np.reshape(field, (field_height, field_width))
+        print field
+        snippet_loc = []
+        for idx in range(field_height):
+            for idy in range(field_width):
+                if field[idx][idy] == 6 :
+                    snippet_loc.append([idx, idy])
+                if field[idx][idy] == int(self.game.my_botid) :
+                    my_loc = [idx, idy]
+                if field[idx][idy] == int(self.game.other_botid) :
+                    opponent_loc = [idx, idy]
 
-        (_, choice) = random.choice(legal)
-        return choice
+        return snippet_loc, my_loc, opponent_loc
+
+
 
     def do_turn(self):
         legal = self.game.field.legal_moves(self.game.my_botid, self.game.players)
@@ -34,8 +43,11 @@ class Bot:
         if len(legal) == 0:
             self.game.issue_order_pass()
         else:
-            choice = self.repeat_previous_move(legal)
-            self.previous = choice
-            self.game.issue_order(choice)
+            snippet, position, opponent = self.get_grid()
+            print snippet
+            print position
+            for coord in snippet:
+                similarity = self.distance_metric(coord, position)
+
 
 
